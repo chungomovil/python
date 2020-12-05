4c7e5f0863798f2037368bcdb742fd27e1d895b1
chungomovil <64377094+chungomovil@users.noreply.github.com>
2020-12-05 14:18:26 +0000

    upload 05-12-2020

--- retomar/22_2.py
+++ retomar/22_2.py
@@ -1,28 +1,25 @@
 """Crear una lista de 5 enteros y cargarlos por teclado. Borrar los elementos mayores o iguales a 10 y generar una nueva lista con dichos valores."""
 
 numeros=[]
+eliminados=[]
 
 for x in range(5):
-    while True:
-        numero=input("Introducir numero: ")
-        try:
-            numero=int(numero)
-            break
-        except ValueError:
-            print("--> Solo valores numericos")
+    numero=int(input("Introducir valor numerico: "))
     numeros.append(numero)
 
-eliminados=[]
+print("\nValores introducidos: %s" % (numeros))
+
 posicion=0
 
-while posicion<len(numeros):
+while posicion<(len(numeros)):
     if numeros[posicion]>=10:
-        print("--> Se ha eliminado el numero",numeros[posicion])
-        eliminados.append(numeros.pop(posicion))
+        print("\nSe ha eliminado el valor %s" % (numeros[posicion]))
+        eliminados.append(numeros[posicion])
+        numeros.pop(posicion)
+
     else:
         posicion+=1
-            
-print("-NUMEROS MENORES A 10-")
-print(numeros)
-print("-NUMEROS MAYORES O IGUALES A 10-")
-print(eliminados)
+
+print("\nValores menores a 10: %s" % (numeros))
+print("\nValores iguales o mayores a 10: %s" % (eliminados))
+
