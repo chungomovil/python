de13f5f47ee8ece6ede638323ff6e24d5faddaf3
chungomovil <64377094+chungomovil@users.noreply.github.com>
2020-12-05 13:18:39 +0000

    upload 05-12-2020

--- retomar/22_1.py
+++ retomar/22_1.py
@@ -1,54 +1,38 @@
-"""
-Crear dos listas paralelas. En la primera ingresar los nombres de empleados y en la segunda los sueldos de cada empleado.
+"""Crear dos listas paralelas. En la primera ingresar los nombres de empleados y en la segunda los sueldos de cada empleado.
 Ingresar por teclado cuando inicia el programa la cantidad de empleados de la empresa.
-Borrar luego todos los empleados que tienen un sueldo mayor a 10000 (tanto el sueldo como su nombre)
-"""
+Borrar luego todos los empleados que tienen un sueldo mayor a 10000 (tanto el sueldo como su nombre)"""
 
-emples=[]
+nombres=[]
 sueldos=[]
-validar=False
+numero=0
+while numero<=0 or numero>=10:
+    numero=int(input("Total de trabajadores: "))
+    if numero<=0 or numero>=10:
+        print("El rango debe de ser entre 1 y 10.")
 
-while validar==False:
-    num=input("Numero de trabajadores: ")
-    validar=num.isdigit()
-    if validar==True:
-        num=int(num)
-    else:
-        print("--> Solo valores superiores a 0")
-
-for x in range(num):
-    validar=False
-    print("- Empleado",x+1,"-")
-    while validar==False:
-        emple=input("Nombre de empleado: ")
-        validar=emple.isalpha()
-        if validar==False:
-            print("--> Solo letras")
-    validar=False
-    while validar==False:
-        sueldo=input("Sueldo: ")
-        validar=sueldo.isdigit()
-        if validar==True:
-            sueldo=int(sueldo)
-        else:
-            print("--> Solo valores igual o supperiores a 0")
-    emples.append(emple)
+for x in range(numero):
+    print("<--DATOS DEL EMPLEADO (%s) -->" % (x+1))
+    nombre=input("Nombre del empleado: ")
+    sueldo=0
+    while sueldo<=50:
+        sueldo=float(input("Sueldo del empleado: "))
+        if sueldo<=50:
+            print("El sueldo debe de ser superior a 50.")
+    sueldo=round(sueldo,2)
+    nombres.append(nombre)
     sueldos.append(sueldo)
 
-posicion=0
+print("\n<--LISTA TOTAL DE EMPLEADOS Y SUELDOS-->")
+print("Nombres: %s" % (nombres))
+print("Sueldos: %s" % (sueldos))
 
-while posicion<len(emples):
+posicion=0
+while posicion<len(nombres):
     if sueldos[posicion]>10000:
-        print("\n--> Se ha eliminado:")
-        print("Empleado:",emples[posicion])
-        print("Sueldo:",sueldos[posicion])
-        emples.pop(posicion)
-        sueldos.pop(posicion)
+        print("\nSe ha eliminado al trabajador %s con un sueldo de %s" % (nombres.pop(posicion),sueldos.pop(posicion)))
     else:
         posicion+=1
-
-print("\n-PLANTILLA FINAL-")
-
-for x in range(len(emples)):
-    print("Nombre:",emples[x])
-    print("Sueldo:",sueldos[x],"\n")
+    
+print("\n<--LISTA DE EMPLEADOS CON SUELDOS INFERIORES A 10.000-->")
+print("Nombres: %s" %(nombres))
+print("Sueldos: %s" % (sueldos))
