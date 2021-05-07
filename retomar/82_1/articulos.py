536745dbe3b8e9aea5e427d9008401895ee9f763
chungomovil <yuaguapo@gmail.com>
2021-05-07 12:22:48 +0100

    upload 07-05-2021 (proyecto-tkinter-mysql)

--- retomar/82_1/articulos.py
+++ retomar/82_1/articulos.py
@@ -8,12 +8,12 @@ def Conectar():
 
 #Funcion para insertar nuevos articulos en la BD
 def Insertar(descripcion, precio):
-    #Conectamos
+    #Conectamos con la BD
     conexion=Conectar()
     #Convertimos en tupla los datos recibidos
     producto=(descripcion, precio)
     #Creamos la consulta
-    consulta=("INSERT INTO articulos (descripcion, precio) VALUES (%s, %s)")
+    consulta="INSERT INTO articulos (descripcion, precio) VALUES (%s, %s)"
     #Creamos el cursor
     cursor1=conexion.cursor()
     #Ejecutamos la consulta con los valores recibidos
@@ -25,10 +25,10 @@ def Insertar(descripcion, precio):
 
 #Funcion para buscar articulo por codigo en la BD
 def Buscar(codigo):
-    #Conectamos
+    #Conectamos con la BD
     conexion=Conectar()
     #Creamos la consulta
-    consulta=("SELECT * FROM articulos WHERE codigo=%s")
+    consulta="SELECT * FROM articulos WHERE codigo=%s"
     #Creamos el cursor
     cursor1=conexion.cursor()
     #Ejecutamos la consulta con el codigo recibido (OJO: Recordar que lo recibimos en una tupla)
@@ -40,7 +40,7 @@ def Buscar(codigo):
 
 #Funcion para mostrar todos los articulos de la BD
 def Totalidad():
-    #Conectamos
+    #Conectamos con la BD
     conexion=Conectar()
     #Creamos cursor
     cursor1=conexion.cursor()
@@ -50,3 +50,46 @@ def Totalidad():
     conexion.close()
     #Retornamos todos los campos de la consulta
     return cursor1.fetchall()
+
+#Funcion para borrar los articulos de la BD
+def Borrar(codigo):
+    #Buscamos y almacenamos en una variable la informacion del articulo antes de que se elimine
+    articulo=Buscar(codigo)
+    #Conectamos con la BD
+    conexion=Conectar()
+    #Creamos la consulta de eliminacion
+    consulta="DELETE FROM articulos WHERE codigo=%s"
+    #Creamos el cursor
+    cursor1=conexion.cursor()
+    #Ejecutamos la consulta
+    cursor1.execute(consulta, codigo)
+    #Fijamos los cambios
+    conexion.commit()
+    #Cerramos conexion
+    conexion.close()
+    #Retornamos informacion del articulo eliminado
+    return articulo
+
+#Funcion para modificar articulo de la BD
+def Modificar(codigo, descripcion, precio):
+    #Conectamos con la BD
+    conexion=Conectar()
+    #Creamos el cursor
+    cursor1=conexion.cursor()
+    #Controlamos si no esta vacio el campo
+    if descripcion!="":
+        #Creamos la consulta de modificacion
+        consulta="UPDATE articulos SET descripcion=%s WHERE codigo=%s"
+        #Ejecutamos la consulta
+        cursor1.execute(consulta, (descripcion, codigo))
+    if precio!="":
+        consulta="UPDATE articulos SET precio=%s WHERE codigo=%s"
+        cursor1.execute(consulta,(precio, codigo))
+    #Fijamos los cambios
+    conexion.commit()
+    #Cerramos la conexion
+    conexion.close()
+    #Buscamos y almacenamos los datos del articulo modificado
+    articulo=Buscar((codigo, ))
+    #Retornamos los datos del articulo modificado
+    return articulo
--- retomar/82_1/formulario_articulos.py
+++ retomar/82_1/formulario_articulos.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext as st
 from tkinter import messagebox as mb
+import sys
 #Importamos nuestro propio modulo para la sintaxis sql
 import articulos
 
@@ -14,6 +15,12 @@ class FormularioArticulos:
         #Creamos la ventana
         self.ventana=tk.Tk()
         self.ventana.title("MANTENIMIENTO DE ARTICULOS")
+        #Creamos el menu con la opcion de salir
+        menubar=tk.Menu(self.ventana)
+        self.ventana.configure(menu=menubar)
+        opciones1=tk.Menu(menubar, tearoff=0)
+        opciones1.add_command(label="SALIR", command=self.Salir)
+        menubar.add_cascade(label="Opciones", menu=opciones1)
         #Creamos el notebook para las opciones
         self.notebook=ttk.Notebook(self.ventana)
         self.notebook.grid(column=0, row=0, padx=10, pady=10)
@@ -21,7 +28,10 @@ class FormularioArticulos:
         self.CamposArticulo()
         self.CamposCodigo()
         self.CamposListado()
+        self.CamposBorrado()
+        self.CamposModificar()
         #Propiedades de la ventana
+        self.ventana.minsize(570,320)
         self.ventana.mainloop()
 
     #Metodo para crear formulario de ingresar articulo nuevo
@@ -90,6 +100,54 @@ class FormularioArticulos:
         self.scrolledtext1=st.ScrolledText(self.labelframe3, width=35, height=10)
         self.scrolledtext1.grid(column=0, row=1, padx=5, pady=10)
 
+    #Metodo para crear el formulario para borrar articulos
+    def CamposBorrado(self):
+        #Agreamos un nuevo frame al notebook
+        self.frame4=ttk.Frame(self.notebook)
+        self.notebook.add(self.frame4, text="Borrado de articulos")
+        #Creamos el labelframe contenedor
+        self.labelframe4=tk.LabelFrame(self.frame4, text="Articulo")
+        self.labelframe4.grid(column=0, row=0, padx=10, pady=10)
+        #Creamos los campos del formulario
+        self.etiqueta6=ttk.Label(self.labelframe4, text="Codigo:")
+        self.etiqueta6.grid(column=0, row=0, padx=5, pady=10)
+        self.dato6=tk.StringVar()
+        self.entrada6=ttk.Entry(self.labelframe4, width=20, textvariable=self.dato6)
+        self.entrada6.grid(column=1, row=0, padx=5, pady=10)
+        #Boton para borrar el articulo
+        self.boton4=ttk.Button(self.labelframe4, width=10, text="Borrar", command=self.BorrarArticulo)
+        self.boton4.grid(column=1, row=1, padx=5, pady=10)
+
+    #Metodo para crear el formulario de modificar articulos
+    def CamposModificar(self):
+        #Agregamos un nuevo frame al notebook
+        self.frame5=ttk.Frame(self.notebook)
+        self.notebook.add(self.frame5, text="Modificar articulo")
+        #Creamos el labelframe contenedor
+        self.labelframe5=tk.LabelFrame(self.frame5, text="Articulo")
+        self.labelframe5.grid(column=0, row=0, padx=10, pady=10)
+        #Creamos los campos del formulario
+        self.etiqueta7=ttk.Label(self.labelframe5, text="Codigo:")
+        self.etiqueta7.grid(column=0, row=0, padx=5, pady=10)
+        self.dato7=tk.StringVar()
+        self.entrada7=ttk.Entry(self.labelframe5, width=20, textvariable=self.dato7)
+        self.entrada7.grid(column=1, row=0, padx=5, pady=10)
+        self.etiqueta8=ttk.Label(self.labelframe5, text="Descripcion:")
+        self.etiqueta8.grid(column=0, row=1, padx=5, pady=10)
+        self.dato8=tk.StringVar()
+        self.entrada8=ttk.Entry(self.labelframe5, width=20, textvariable=self.dato8)
+        self.entrada8.grid(column=1, row=1, padx=5, pady=10)
+        self.etiqueta9=ttk.Label(self.labelframe5, text="Precio:")
+        self.etiqueta9.grid(column=0, row=2, padx=5, pady=10)
+        self.dato9=tk.StringVar()
+        self.entrada9=ttk.Entry(self.labelframe5, width=20, textvariable=self.dato9)
+        self.entrada9.grid(column=1, row=2, padx=5, pady=10)
+        #Creamos los botones para modificar y consultar articulos
+        self.boton5=ttk.Button(self.labelframe5, width=10, text="Consultar", command=self.ConsultarArticulo)
+        self.boton5.grid(column=1, row=3, padx=5, pady=10)
+        self.boton6=ttk.Button(self.labelframe5, width=10, text="Modificar", command=self.ModificarArticulo)
+        self.boton6.grid(column=1, row=4, padx=5, pady=10)
+
     #Metodo para insertar un nuevo articulo en la BD
     def InsertarArticulo(self):
         #Obtenemos los datos del formulario
@@ -130,6 +188,51 @@ class FormularioArticulos:
             #Insertamos articulos en el scrolledtext (OJO: partimos desde 'tk.END' para que se muestras en orden, de lo contrario se mostrarian a la inversa)
             self.scrolledtext1.insert(tk.END, producto)
 
+    #Metodo para borrar articulos
+    def BorrarArticulo(self):
+        #Obtenemos el codigo del articulo y lo pasamos a una tupla para sql
+        codigo=(self.dato6.get(), )
+        #Retornamos los datos del articulo eliminado
+        articulo=articulos.Borrar(codigo)
+        #Mostramos los mensajes de confirmacion
+        if len(articulo)>0:
+            mb.showinfo("Informacion", "Se ha eliminado correctamente el articulo: '"+str(articulo[0][1])+"'.")
+        else:
+            mb.showerror("ERROR", "El articulo no existe.")
+
+    #Metodo para modificar articulos
+    def ModificarArticulo(self):
+        #Obtenemos los datos del articulo
+        codigo=self.dato7.get()
+        descripcion=self.dato8.get()
+        precio=self.dato9.get()
+        #Mostramos mensajes de confirmacion
+        if codigo=="":
+            mb.showerror("ERROR", "El campo 'codigo' esta vacio.")
+        else:
+            articulo=articulos.Modificar(codigo, descripcion, precio)
+            if len(articulo)>0:
+                mb.showinfo("Informacion", "Se ha modificado correctamente el articulo: '"+str(articulo[0][1])+"'.")
+            else:
+                mb.showerror("ERROR", "El articulo no existe.")
+
+    #Metodo para consultar articulo por su codigo
+    def ConsultarArticulo(self):
+        #Obtenemos el codigo del articulo y lo pasamos a una tupla para sql
+        codigo=(self.dato7.get(), )
+        #Obtenemos datos del articulo
+        articulo=articulos.Buscar(codigo)
+        #Insertamos datos en las variables de texto los distintos entry
+        if len(articulo)>0:
+            self.dato8.set(articulo[0][1])
+            self.dato9.set(articulo[0][2])
+        #Mostramos mensajes de confirmacion
+        else:
+            mb.showerror("ERROR", "El articulo no existe.")
+
+    #Metodo para salir del programa
+    def Salir(self):
+        sys.exit()
+
 #Bloque principal y creamos de objeto
 iniciar=FormularioArticulos()
-
